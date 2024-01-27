f947fd91b48c0c0fef235c6447c96822a75f6c1f
Cheder-one <cherbadenis1@gmail.com>
2024-01-27 23:58:56 +0300

    import 'core-js';

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'core-js';
 
 import App from './App.jsx';
 import './styles/index.scss';
